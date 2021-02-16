59686c96a85fea8c03f592a5b264fc88f68b6581
thm2kor <76072427+thm2kor@users.noreply.github.com>
2021-02-16 15:05:34 +0100

    debug obstacle map added

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -18,6 +18,8 @@ result_file = '../images/'
 debug_data_init = '../debug/parameter-init.txt'
 debug_data_update = '../debug/parameter-after-updateweights.txt'
 debug_data_resample = '../debug/parameter-after-resample.txt'
+debug_data_best_particle = '../debug/paramter-best-particles.txt'
+
 
 num_particles = 100
 total_time_steps = 15
@@ -29,7 +31,7 @@ def plot_map_data():
     obs_map_x = (map_data[:,0]).astype(np.float)
     obs_map_y = (map_data[:,1]).astype(np.float) 
     
-    ## Read map data and draw the obstacles in red
+    # Read map data and draw the obstacles in red
     #init_data =  list(csv.reader(open(debug_data_init)))
     #init_data = np.array(init_data)
     #x = (init_data[:,1]).astype(np.float)
@@ -37,12 +39,19 @@ def plot_map_data():
     #theta = (init_data[:,3]).astype(np.float) 
     #weight = (init_data[:,4]).astype(np.float)
     
-    fig = plt.figure()
+    fig = plt.figure(figsize=(28, 16))
+    plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
     ax1 = fig.add_subplot(111)
-    ax1.scatter(obs_map_x,obs_map_y, s=10, c='r', marker="s")
+    ax1.scatter(obs_map_x,obs_map_y, s=100, c='r', marker="s")
     #ax1.scatter(x,y, s=2, c='b', marker="o")
+    plt.savefig(result_file + 'init_chart.jpg')
+
+    print ('Init map stored at ' + result_file + 'init_chart.jpg' )
     
 def plot_particles_stage2():
+    plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
     # plot the points for all the time stamps
     update_data =  list(csv.reader(open(debug_data_update)))
     update_data = np.array(update_data)
@@ -64,7 +73,7 @@ def plot_particles_stage2():
         
         #prepare the color map
         cmap = mcolors.ListedColormap(plt.rcParams['axes.prop_cycle'].by_key()['color'])
-
+        
         #plot the scatter charts - 1 - shows the particles after weights update
         fig, (ax_update, ax_resample) = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True,figsize=(28, 12))
         im_update = ax_update.scatter(x_update, y_update, s=100, c=weight_update, cmap=cmap,  marker="o")
@@ -86,11 +95,18 @@ def plot_particles_stage2():
 
     
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Compare particles ')
-    parser.add_argument('--timesteps', dest='time_steps' , default=5)
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Visualize the output from the Particle filter algorithm')
+    parser.add_argument('--timesteps', type=int, dest='time_steps' , default=5)
+    parser.add_argument('--show_map_observation', action='store_true', dest='show_map_observation' , default=False)
+    parser.add_argument('--show_particle_updates', action='store_true', dest='show_particle_updates' , default=False)
+
+    args =  parser.parse_args()
     total_time_steps = args.time_steps
     
-    plot_particles_stage2()
-    print ('Results stored at images folder (../images/plot_at_time_*.jpg)' )
+    if args.show_map_observation:
+        plot_map_data()
+        
+    if args.show_particle_updates:
+        plot_particles_stage2()
+        print ('Results stored at images folder (../images/plot_at_time_*.jpg)' )
     
